0a5df4dafca4223f1143de9bc79c71090fd2fe98
Hafijur Rahman <hafijur021998@gmail.com>
2024-02-27 20:56:09 -0800

    practice set 2

---
+++ 02_practice_set/script.js
@@ -0,0 +1,11 @@
+// 1. At a restaurant you ate: 1 Dal Rs 100, 2 Roti Rs 10 each, 1 Ice Cream Rs 30, Calculate and display the final bill amount.
+
+// 2. Calculate 18% GST on iPhone15 Rs.79,990 and 2 Air pods Pro Rs.24,990 each.
+
+// 3. Create a string using 3 all methods.
+
+// 4. Concatenate string with string, and string with numbers.
+
+// 5. Create order summary string for our myntra cart.
+
+// 6. Display order summary in a popup
