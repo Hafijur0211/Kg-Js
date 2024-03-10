ede1cfad361da45f43134faf8f01791eb93d653a
Hafijur Rahman <hafijur021998@gmail.com>
2024-03-10 20:09:26 +0530

    chapter-2-question-3

--- 02_practice_set/script.js
+++ 02_practice_set/script.js
@@ -12,6 +12,7 @@ console.log("Final bill amount : Rs " + totalBill);
 
 // 2. Calculate 18% GST on iPhone15 Rs.79,990 and 2 Air pods Pro Rs.24,990 each.
 
+/*
 const iPhonePrice = 79990;
 const airPodsPrice = 24990;
 
@@ -20,8 +21,15 @@ const totalPriceBeforeGST = iPhonePrice + 2 * airPodsPrice;
 const getAmount = totalPriceBeforeGST * 0.18;
 
 console.log("Get amount : " + getAmount);
+*/
 
 // 3. Create a string using 3 all methods.
+
+let string1 = "This is single quotes string";
+
+let string2 = "This is double qoutes string";
+
+let string3 = new String("This is string created using the string constructor");
 
 // 4. Concatenate string with string, and string with numbers.
 
