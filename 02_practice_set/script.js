b101cd5bd21c2c258ab0a32e1999d17e5c00494d
Hafijur Rahman <hafijur021998@gmail.com>
2024-03-10 20:14:26 +0530

    chapter-2-question-4

--- 02_practice_set/script.js
+++ 02_practice_set/script.js
@@ -25,13 +25,29 @@ console.log("Get amount : " + getAmount);
 
 // 3. Create a string using 3 all methods.
 
+/*
 let string1 = "This is single quotes string";
 
 let string2 = "This is double qoutes string";
 
 let string3 = new String("This is string created using the string constructor");
+*/
 
 // 4. Concatenate string with string, and string with numbers.
+
+// concatenate string with string
+
+let string1 = "Hello"
+let string2 = "world"
+
+let resultString = string1 + " " + string2
+console.log(resultString)
+
+// concatenate string with number
+
+let number  = 98;
+let string = "The answer is " + number;
+console.log(string)
 
 // 5. Create order summary string for our myntra cart.
 
