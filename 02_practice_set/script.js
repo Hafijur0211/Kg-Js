53be62a6e43fce6756ba5a49920f69f6301893d5
Hafijur Rahman <hafijur021998@gmail.com>
2024-03-10 10:44:24 +0530

    chapter-2-question-2

--- 02_practice_set/script.js
+++ 02_practice_set/script.js
@@ -1,5 +1,6 @@
 // 1. At a restaurant you ate: 1 Dal Rs 100, 2 Roti Rs 10 each, 1 Ice Cream Rs 30, Calculate and display the final bill amount.
 
+/*
 const dalPrice = 100;
 const rotiPrice = 10;
 const icecreamPrice = 30;
@@ -7,8 +8,18 @@ const icecreamPrice = 30;
 const totalBill = dalPrice + 2 * rotiPrice + icecreamPrice;
 
 console.log("Final bill amount : Rs " + totalBill);
+*/
 
 // 2. Calculate 18% GST on iPhone15 Rs.79,990 and 2 Air pods Pro Rs.24,990 each.
+
+const iPhonePrice = 79990;
+const airPodsPrice = 24990;
+
+const totalPriceBeforeGST = iPhonePrice + 2 * airPodsPrice;
+
+const getAmount = totalPriceBeforeGST * 0.18;
+
+console.log("Get amount : " + getAmount);
 
 // 3. Create a string using 3 all methods.
 
